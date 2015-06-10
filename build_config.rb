bfcaa662e1dbb135a86dee2005fd9feaf64d7d8a
qipeng <qipeng.zhang0818@gmail.com>
2015-06-10 15:51:25 +0900

    Update build_config.rb
    add cross-compile for ARM

--- build_config.rb
+++ build_config.rb
@@ -83,42 +83,46 @@ MRuby::Build.new do |conf|
   # conf.enable_bintest
 end
 
-MRuby::Build.new('host-debug') do |conf|
-  # load specific toolchain settings
-
-  # Gets set by the VS command prompts.
-  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
-  else
-    toolchain :gcc
-  end
-
-  enable_debug
-
-  # include the default GEMs
-  conf.gembox 'default'
-
-  # C compiler settings
-  conf.cc.defines = %w(ENABLE_DEBUG)
-
-  # Generate mruby debugger command (require mruby-eval)
-  conf.gem :core => "mruby-bin-debugger"
-
-  # bintest
-  # conf.enable_bintest
-end
+#MRuby::Build.new('host-debug') do |conf|
+#  # load specific toolchain settings
+#
+#  # Gets set by the VS command prompts.
+#  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+#    toolchain :visualcpp
+#  else
+#    toolchain :gcc
+#  end
+#
+#  enable_debug
+#
+#  # include the default GEMs
+#  conf.gembox 'default'
+#
+#  # C compiler settings
+#  conf.cc.defines = %w(ENABLE_DEBUG)
+#
+#  # Generate mruby debugger command (require mruby-eval)
+#  conf.gem :core => "mruby-bin-debugger"
+#
+#  # bintest
+#  # conf.enable_bintest
+#end
 
 # Define cross build settings
-# MRuby::CrossBuild.new('32bit') do |conf|
-#   toolchain :gcc
-#
-#   conf.cc.flags << "-m32"
-#   conf.linker.flags << "-m32"
-#
-#   conf.build_mrbtest_lib_only
-#
-#   conf.gem 'examples/mrbgems/c_and_ruby_extension_example'
-#
-#   conf.test_runner.command = 'env'
-#
-# end
+MRuby::CrossBuild.new('arm') do |conf|
+   toolchain :arm
+
+   enable_debug
+
+   conf.gembox 'default'
+
+   #conf.cc.flags << "-m32"
+   #conf.linker.flags << "-m32"
+
+   conf.build_mrbtest_lib_only
+
+   conf.gem 'examples/mrbgems/c_and_ruby_extension_example'
+
+   conf.test_runner.command = 'env'
+
+end
